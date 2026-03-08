2f8fd15461f1d0ebc8b2642b017369e172489514
AntonioBDev <basi.dev9@gmail.com>
2026-03-07 23:54:16 -0600

    Actualizando secciones

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,4 +1,5 @@
 import Carousel from "../components/carrusel";
+import IdealCustomer from "../components/idealCustomer";
 import BotonPago from "../components/botonpago";
 import Aprendizaje from "../components/aprendizaje";
 import Resultados from "../components/resultados";
@@ -9,18 +10,16 @@ export default function LandingPage() {
   return (
     <section>
       <header className="max-w-6xl mx-auto px-4">
-        <nav className="p-5 flex justify-between items-center luz-neon-borde fixed top-0 left-0 w-full bg-black opacity-98 z-10">
-          <img src="./public/logo.jpeg" className="h-20" alt="" />
-
-          <BotonPago />
+        <nav className="p-5 flex justify-between items-center luz-neon-borde fixed top-0 left-0 w-full bg-slate-950 opacity-98 z-10">
+          <img src="./public/logo.png" className="h-20" alt="" />
         </nav>
 
-        <section className=" from-purple-700 via-purple-800 to-purple-900 text-white py-16 px-6 text-center mt-24">
+        <section className=" from-purple-700 via-purple-800 to-purple-900 py-16 px-6 text-center mt-24">
           <h1 className="text-4xl md:text-5xl font-extrabold leading-tight max-w-4xl mx-auto">
-            Domina Facebook Ads y escala tus ventas sin quemar tu presupuesto en
+            Domina <span className="text-purple-600"> Facebook Ads </span>  y escala tus ventas sin quemar tu presupuesto en
             publicidad.
           </h1>
-          <p className="mt-6 text-lg md:text-xl max-w-3xl mx-auto">
+          <p className="mt-6 text-lg md:text-xl max-w-3xl mx-auto text-slate-400">
             Aprende el método exacto para crear campañas de alto impacto,
             segmentar a tu cliente ideal y convertir clics en clientes
             recurrentes, aunque nunca hayas abierto el Administrador de
@@ -55,58 +54,7 @@ export default function LandingPage() {
         <Carousel />
       </section>
 
-      <main className="max-w-6xl mx-auto px-4">
-        <h2 className="mt-10 mb:10 uppercase text-3xl md:text-4xl font-extrabold text-center ">
-          ¿Para quien es esto?
-        </h2>
-        <div className="grid md:grid-cols-3 gap-4 mb-8">
-          <div className=" rounded-2xl p-4">
-            <p className="md:mt-6 text-lg md:text-xl max-w-3xl mx-auto text-center text-purple-600 font-bold uppercase">
-              Emprendedores
-            </p>
-            <p className="text-lg md:text-xl max-w-3xl mx-auto text-center">
-              Que se sienten frustrados porque sus anuncios no venden.
-            </p>
-            <div className="w-full flex justify-center items-center">
-              <img
-                src="./src/assets/emprendedores.svg"
-                alt=""
-                className="w-60 mt-6"
-              />
-            </div>
-            {/* <i className="fa-solid fa-"></i> */}
-          </div>
-
-          <div className=" rounded-2xl p-4">
-            <p className="mt-6 text-lg md:text-xl max-w-3xl mx-auto text-center text-purple-600 font-bold uppercase">
-              Negocios locales
-            </p>
-            <p className="text-lg md:text-xl max-w-3xl mx-auto text-center">
-              Que quieren clientes nuevos todos los días.
-            </p>
-            <div className="w-full flex justify-center items-center">
-              <img src="./src/assets/tienda.svg" alt="" className="w-60 mt-6" />
-            </div>
-          </div>
-
-          <div className=" rounded-2xl p-4">
-            <p className="mt-6 text-lg md:text-xl max-w-3xl mx-auto text-center text-purple-600 font-bold uppercase">
-              Personas desde cero
-            </p>
-            <p className="text-lg md:text-xl max-w-3xl mx-auto text-center">
-              Que tienen miedo de tocar el Administrador de Anuncios y romper
-              algo.
-            </p>
-            <div className="w-full flex justify-center items-center">
-              <img
-                src="./src/assets/principiantes.svg"
-                alt=""
-                className="w-60 mt-6"
-              />
-            </div>
-          </div>
-        </div>
-      </main>
+      <IdealCustomer/>
 
       <div className="container mx-auto px-6">
         <Aprendizaje />
