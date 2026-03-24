724af7b10a9254c20085c921f8c46b40a9e5c999
AntonioBDev <basi.dev9@gmail.com>
2026-03-24 14:20:57 -0600

    stickyBar

--- src/components/notes/StickyBar.jsx
+++ src/components/notes/StickyBar.jsx
@@ -14,9 +14,9 @@ export default function StickyBar() {
             onclick="return false;"
             target='_blank'
             href="https://pay.hotmart.com/N104199019V?checkoutMode=2"
-            className=" bg text-white text-xl md:text-2xl p-2 rounded-3xl font-extrabold shadow-md hover:bg-purple-700 "
+            className=" bg text-white text-xl md:text-2xl p-2 rounded-3xl font-extrabold shadow-md hover:bg-purple-700 capitalize"
           >
-            Unete ahora
+            Accede ahora
           </a>
       </div>
     </div>
