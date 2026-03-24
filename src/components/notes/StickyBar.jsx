b2e239096c4c2e8a31fd135fd76552b79fc05575
AntonioBDev <basi.dev9@gmail.com>
2026-03-24 14:18:10 -0600

    stickyBar cambios

--- src/components/notes/StickyBar.jsx
+++ src/components/notes/StickyBar.jsx
@@ -3,16 +3,21 @@ import Boton from '../common/Button';
 
 export default function StickyBar() {
   return (
-    <div className="fixed bottom-5 left-5 right-5 w-auto z-50 bg-slate-900/95 backdrop-blur-md border rounded-3xl border-purple-500/30 p-4 md:p-6 shadow-[0_-10px_40px_rgba(0,0,0,0.5)]">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-4">
+    <div className="fixed bottom-2 left-2 right-2 w-auto z-50 bg-slate-900/95 backdrop-blur-md border rounded-3xl border-purple-500/30 p-4 md:p-6 shadow-[0_-10px_40px_rgba(0,0,0,0.5)]">
+      <div className="container mx-auto flex md:flex-row items-center justify-between gap-4">
         
-        {/* Información de la Oferta */}
         <div className="text-center md:text-left">
-          <p className="text-white text-lg md:text-xl font-bold flex items-center">
-            Inscríbete al curso por solo $<span className=' line-through text-sm md:text-lg mr-2'>109 </span> a <span className="text-green-400 text-3xl md:text-4xl ml-2"> $25</span>
+          <p className="text-white text-2xl md:text-3xl font-bold flex items-center">$<span className=' line-through'>69</span> <span className="text-degradient2  text-3xl md:text-5xl ml-2 font-extrabold"> $25</span>
           </p>
         </div>
-        <Boton/>
+            <a
+            onclick="return false;"
+            target='_blank'
+            href="https://pay.hotmart.com/N104199019V?checkoutMode=2"
+            className=" bg text-white text-xl md:text-2xl p-2 rounded-3xl font-extrabold shadow-md hover:bg-purple-700 "
+          >
+            Unete ahora
+          </a>
       </div>
     </div>
   )
