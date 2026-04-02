97f02d00306036bd76b96ec6b3f645968e9b8448
AntonioBDev <basi.dev9@gmail.com>
2026-04-01 22:48:49 -0600

    Video optimizacion

--- src/components/common/Video.jsx
+++ src/components/common/Video.jsx
@@ -5,20 +5,14 @@ export default function Video() {
   return (
     <div className="relative mt-10 md:mt-20 mb-10 px-4 flex justify-center">
       <div className="w-full max-w-[400px] md:max-w-[350px] lg:max-w-[400px]">
-        {/* Explicación: 
-           En móvil usamos max-w-[400px] para que no pegue a los bordes si la pantalla es grande.
-           En Desktop (md:) limitamos el ancho a ~350px-400px porque al ser vertical, 
-           si le das mucho ancho, la altura se vuelve infinita.
-        */}
         <video
-          autoPlayn
-          loop
+          autoPlay
           muted
-          playsInline // 👈 CRÍTICO: Evita que el video se abra en pantalla completa
-          preload="auto" // 👈 AYUDA: Indica al navegador que empiece a descargar ya
+          loop
+          playsInline
+          controlsList="nodownload"
           controls
-          controlsList="nodownload noremoteplayback"
-          onContextMenu={(e) => e.preventDefault()}
+          preload="auto" // 👈 AYUDA: Indica al navegador que empiece a descargar ya
           className="w-full h-auto rounded-2xl md:rounded-3xl shadow-[0_0_30px_rgba(82,9,154,0.5)] border border-purple-500/20"
         >
           <source src={video} type="video/mp4" />
