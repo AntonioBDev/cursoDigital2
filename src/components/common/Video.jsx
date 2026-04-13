19a09fd5b6d04a17cff7d9a1be0f9c06ceffb471
AntonioBDev <basi.dev9@gmail.com>
2026-04-12 21:29:36 -0600

    Actualizacion para modo vertical

--- src/components/common/Video.jsx
+++ src/components/common/Video.jsx
@@ -4,7 +4,7 @@ import video from "../../assets/video/presentation.mp4";
 export default function Video() {
   return (
     <div className="relative mt-10 md:mt-20 mb-10 px-4 flex justify-center">
-      <div className="w-full max-w-[400px] md:max-w-[350px] lg:max-w-[400px]">
+      <div className="w-full max-w-[350px] md:max-w-[450px] ">
         <video
           autoPlay
           muted
@@ -13,7 +13,7 @@ export default function Video() {
           controlsList="nodownload"
           controls
           preload="auto" // 👈 AYUDA: Indica al navegador que empiece a descargar ya
-          className="w-full h-auto rounded-2xl md:rounded-3xl shadow-[0_0_30px_rgba(82,9,154,0.5)] border border-purple-500/20"
+          className="w-full aspect-[9/16] h-auto rounded-2xl md:rounded-3xl shadow-[0_0_30px_rgba(82,9,154,0.5)] border border-purple-500/20"
         >
           <source src={video} type="video/mp4" />
           Tu navegador no soporta videos.
